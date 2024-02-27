dd69257e601e969f63dd98d614ff6b9f6ffd601e
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2024-02-27 10:34:09 -0300

    [FIX] Fixing context creation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,9 +9,27 @@ function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
   const [tasks, setTasks] = useState([
-    { id: 1, title: "Tarefa 1", description: "Descrição da tarefa 1", priority: "Alta", status: "Aberta" },
-    { id: 2, title: "Tarefa 2", description: "Descrição da tarefa 2", priority: "Média", status: "Disponível" },
-    { id: 3, title: "Tarefa 3", description: "Descrição da tarefa 3", priority: "Baixa", status: "Fechada" },
+    {
+      id: 1,
+      title: "Tarefa 1",
+      description: "Descrição da tarefa 1",
+      priority: "Alta",
+      status: "Aberta",
+    },
+    {
+      id: 2,
+      title: "Tarefa 2",
+      description: "Descrição da tarefa 2",
+      priority: "Média",
+      status: "Disponível",
+    },
+    {
+      id: 3,
+      title: "Tarefa 3",
+      description: "Descrição da tarefa 3",
+      priority: "Baixa",
+      status: "Fechada",
+    },
   ]);
 
   const navigate = useNavigate();
@@ -31,26 +49,31 @@ function Home() {
   };
 
   return (
-    <div className="home-container">
-      <Header/>
-      <div className="surface-card">
-        <div className="text-center mb-5">
-          <div className="text-900 text-3xl font-medium mb-3">
-            Lista de Tarefas
+    <>
+      <Header />
+      <div className="home-container">
+        <div className="surface-card">
+          <div className="text-center mb-5">
+            <div className="text-900 text-3xl font-medium mb-3">
+              Lista de Tarefas
+            </div>
+          </div>
+
+          <div className="tasks-container">
+            {tasks.map((task) => (
+              <div
+                className="task-card"
+                key={task.id}
+                onClick={() => handleTaskClick(task)}
+              >
+                <Task taskInfo={task} />
+              </div>
+            ))}
           </div>
         </div>
-
-        <div className="tasks-container">
-          {tasks.map((task) => (
-            <div className="task-card" key={task.id} onClick={() => handleTaskClick(task)}>
-              <Task taskInfo={task} />
-            </div>
-          ))}
-        </div>
-
+        {isModalOpen && <Modal taskInfo={selectedTask} onClose={closeModal} />}
       </div>
-      {isModalOpen && <Modal taskInfo={selectedTask} onClose={closeModal} />}
-    </div>
+    </>
   );
 }
 
--- src/pages/MainHome/MainHome.jsx
+++ src/pages/MainHome/MainHome.jsx
@@ -7,10 +7,11 @@ import './MainHome.scss';
 import { useTasks } from '../../contexts/TasksContext';
 
 function MainHome() {
-  const { tasks} = useTasks();
+  const { tasks } = useTasks();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
   const [filter, setFilter] = useState('all');
+  const [filterIndex, setFilterIndex] = useState(0);
 
   const handleTaskClick = (taskInfo) => {
     setSelectedTask(taskInfo);
@@ -23,19 +24,21 @@ function MainHome() {
 
   const handleFilterChange = (e) => {
     setFilter(e.value);
+    setFilterIndex(filterItems.findIndex(item => item.value === e.value));
+    console.log("Filter value:", e.value); // Verifica o valor do filtro no console
   };
 
   const filteredTasks = tasks.filter((task) => {
-    if (filter === 'all') {
-      return true;
-    } else if (filter === 'abertas') {
-      return task.status === 'Aberta';
-    } else if (filter === 'fechadas') {
-      return task.status === 'Fechada';
-    } else if (filter === 'disponiveis') {
-      return task.status === 'Disponível';
+    switch (filter.value) { // Correção aqui: Use filter.value em vez de filter
+      case 'abertas':
+        return task.status === 'Aberta';
+      case 'fechadas':
+        return task.status === 'Fechada';
+      case 'disponiveis':
+        return task.status === 'Disponível';
+      default:
+        return true; // Para o filtro 'all' ou qualquer outro valor desconhecido
     }
-    return true;
   });
 
   const filterItems = [
@@ -45,13 +48,15 @@ function MainHome() {
     { label: 'Disponíveis', value: 'disponiveis' },
   ];
 
+  console.log("Filtered tasks:", filteredTasks); // Verifica as tarefas filtradas no console
+
   return (
     <>
       <Header />
       <main className="home-container">
         <h1 className="home-title">Lista de Tarefas</h1>
         <div className="filter-buttons">
-          <TabMenu model={filterItems} activeItem={filter} onTabChange={handleFilterChange} />
+          <TabMenu model={filterItems} activeIndex={filterIndex} onTabChange={handleFilterChange} />
         </div>
         <div className="tasks-container">
           {filteredTasks.map((task) => (
